8b24af23a6025d9a264c5cd4a59ec085adb16538
Ayende Rahien <ayende@ayende.com>
2020-10-26 10:31:31 +0200

    Connecting and reading a document from remote cluster

--- Northwind/Program.cs
+++ Northwind/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using Raven.Client.Documents;
 
 namespace Northwind
 {
@@ -6,7 +8,27 @@ namespace Northwind
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var cert = new X509Certificate2(
+                @"C:\Users\ayende\Downloads\ndc.roll.client.certificate\ndc.roll.client.certificate.pfx");
+            using var store = new DocumentStore
+            {
+                Database = "Demo",
+                Certificate = cert,
+                Urls = new[]
+                {
+                    "https://a.ndc.roll.ravendb.cloud",
+                    "https://b.ndc.roll.ravendb.cloud",
+                    "https://c.ndc.roll.ravendb.cloud"
+                }
+            };
+            store.Initialize();
+
+            using (var session = store.OpenSession())
+            {
+                var employee = session.Load<Employee>("employees/1-A");
+                Console.WriteLine(employee.FirstName);
+            }
+
         }
     }
 }
