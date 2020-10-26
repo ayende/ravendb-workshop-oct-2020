2c3c6cf94b5df2dcb7dad15eb81406ae8803cb3c
Ayende Rahien <ayende@ayende.com>
2020-10-26 16:40:17 +0200

    Working with types

--- Northwind/Program.cs
+++ Northwind/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 using Raven.Client.Documents.Linq;
 using System.Security.Cryptography.X509Certificates;
 using Raven.Client.Documents;
@@ -37,38 +38,138 @@ namespace Northwind
             };
             store.Initialize();
 
-            new Employees_SearchNotes().Execute(store);
+            IndexCreation.CreateIndexes(Assembly.GetExecutingAssembly(), store);
+            //using (var session = store.OpenSession())
+            //using(var bulk = store.BulkInsert())
+            //{
+            //    foreach (var i in session.Query<Employee>().ToList())
+            //    {
+            //        bulk.Store(i, i.Id);
+            //    }
+            //    foreach (var i in session.Query<Supplier>().ToList())
+            //    {
+            //        bulk.Store(i, i.Id);
+            //    }
+            //    foreach (var i in session.Query<Company>().ToList())
+            //    {
+            //        bulk.Store(i, i.Id);
+            //    }
+            //    return;
+            //}
 
             using (var session = store.OpenSession())
             {
-                var results = session.Query<Employees_SearchNotes.Result, Employees_SearchNotes>()
-                    .Search(x => x.Name, "Anne")
-                    .OfType<Employee>()
+                var ravenQueryable = session.Query<People_Search.Result, People_Search>()
+                    //.Where(x=>x.Name == "anne")
+                    .Search(x => x.Name, "anne")
+                    .Select(x=>new {
+                        Name = x.Contact.Name ?? x.FirstName + " " + x.LastName
+                    });
+                Console.WriteLine(ravenQueryable);
+                var results = ravenQueryable
                     .ToList();
+
                 foreach (var result in results)
                 {
-                    Console.WriteLine(result.FirstName);
+                    Console.WriteLine(result);
+                    //switch (result)
+                    //{
+                    //    case Employee e:
+                    //        Console.WriteLine(e.FirstName + " " + e.LastName);
+                    //        break;
+                    //    case Supplier s:
+                    //        Console.WriteLine(s.Contact.Name);
+                    //        break;
+                    //    case Company c:
+                    //        Console.WriteLine(c.Contact.Name);
+                    //        break;
+                    //}
                 }
-
             }
 
-            //var sp = Stopwatch.StartNew();
             //using (var session = store.OpenSession())
             //{
-            //    var results = session.Query<Company>()
-            //        .Search(x => x.Contact.Name, "Karl")
+            //    var results = session.Query<Orders_TotalCost.Result, Orders_TotalCost>()
+            //        .Where(x=>x.Total > 100)
+            //        .OfType<Order>()
             //        .ToList();
             //    foreach (var result in results)
             //    {
-            //        Console.WriteLine(result.Name);
+            //        Console.WriteLine(result.Id);
             //    }
 
-            //}
+                //}
 
-            //Console.WriteLine(sp.Elapsed);
+
+                //using (var session = store.OpenSession())
+                //{
+                //    var results = session.Query<Employees_SearchNotes.Result, Employees_SearchNotes>()
+                //        .Search(x => x.Name, "Anne")
+                //        .OfType<Employee>()
+                //        .ToList();
+                //    foreach (var result in results)
+                //    {
+                //        Console.WriteLine(result.FirstName);
+                //    }
+
+                //}
+
+                //var sp = Stopwatch.StartNew();
+                //using (var session = store.OpenSession())
+                //{
+                //    var results = session.Query<Company>()
+                //        .Search(x => x.Contact.Name, "Karl")
+                //        .ToList();
+                //    foreach (var result in results)
+                //    {
+                //        Console.WriteLine(result.Name);
+                //    }
+
+                //}
+
+                //Console.WriteLine(sp.Elapsed);
         }
     }
 
+    public class People_Search : AbstractMultiMapIndexCreationTask<People_Search.Result>
+    {
+        public class Result
+        {
+            public string Name;
+
+            public Contact Contact;
+            public string LastName, FirstName;
+        }
+
+        public People_Search()
+        {
+            AddMap<Employee>(employees =>
+                from employee in employees 
+                select new {Name = new[] {employee.FirstName, employee.LastName}});
+            AddMap<Company>(companies => from company in companies
+                                         select new {company.Contact.Name});
+            AddMap<Supplier>(suppliers => from supplier in suppliers
+                select new { supplier.Contact.Name });
+            Index(x=>x.Name, FieldIndexing.Search);
+        }
+    }
+
+    public class Orders_TotalCost : AbstractIndexCreationTask<Order, Orders_TotalCost.Result>
+    {
+        public class Result
+        {
+            public decimal Total;
+        }
+        public Orders_TotalCost()
+        {
+            Map = orders => from o in orders
+                select new
+                {
+                    Total = o.Lines.Sum(x => x.PricePerUnit * x.Quantity)
+                };
+            Store(x=>x.Total, FieldStorage.Yes);
+        }
+    }
     public class Employees_SearchNotes : AbstractIndexCreationTask<Employee, Employees_SearchNotes.Result>
     {
         public class Result
